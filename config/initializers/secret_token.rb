cefc1c3abc49f1f83ea983fb989dabbf38206fbc
root <root@debian>
2013-11-17 00:51:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FiveControllersApp::Application.config.secret_key_base = '291189969b3995be006ae21c6815d0321ef1097d95205ee95d73d105cc86fc888d976be0eb54b32b7df04e73469480f41c947b4c96aaf011e5ba011a7dc94237'
